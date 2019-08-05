675211988f70e0cae65b6fc897ef6eeb2971ee2a
baoqiang <mailbaoqiang@gmail.com>
2019-08-05 13:52:39 +0800

    139 watched movies
    Change-Id: I8ab04603da4aaf12688df5e8d7f2b67ca8a0caae

--- count_sum.py
+++ count_sum.py
@@ -9,37 +9,33 @@
 """
 
 from collections import defaultdict
+import os
 
 
 def count_sum():
     """
     #########################
-    total movies: 174
+    total movies: 168
     #########################
-    要看的电影 -> 26
-    看过的电影 -> 129
-    看过的电视剧 -> 19
+    要看的电影 -> 8
+    看过的电影 -> 140
+    看过的电视剧 -> 20
     #########################
-    看过的电影-动画片 -> 28
+    看过的电影-动画片 -> 33
+    看过的电影-故事片 -> 26
     看过的电影-喜剧片 -> 26
-    看过的电影-故事片 -> 25
-    看过的电影-爱情片 -> 17
+    看过的电影-爱情片 -> 19
     看过的电影-人生片 -> 10
     看过的电影-科幻片 -> 10
     看过的电视剧-武侠剧 -> 9
-    要看的电影-动画片 -> 7
+    要看的电影-故事片 -> 8
     看过的电影-动作片 -> 7
-    要看的电影-故事片 -> 6
-    要看的电影-情色片 -> 4
+    看过的电影-悬疑片 -> 5
     看过的电影-情色片 -> 4
     看过的电视剧-清宫剧 -> 4
     看过的电视剧-爱情剧 -> 4
-    要看的电影-悬疑片 -> 3
-    要看的电影-爱情片 -> 2
-    要看的电影-科幻片 -> 2
-    要看的电影-电视剧 -> 2
-    看过的电影-悬疑片 -> 2
     看过的电视剧-喜剧 -> 2
+    看过的电视剧-奇幻剧 -> 1
     #########################
     """
 
@@ -92,9 +88,67 @@ def count_sum():
     print_sep()
 
 
+def find_diff():
+    """
+    豆瓣看过与readme的不同
+    :return:
+    """
+    home = os.environ['HOME']
+    filenanme = '{}/Downloads/movies.txt'.format(home)
+
+    doubans = set()
+    reads = set()
+
+    with open(filenanme, 'r', encoding='utf-8') as f:
+        name = ''
+
+        for line in f:
+            if '/' in line:
+                name = line.strip().split('/')[0]
+            elif '[' in line:
+                name = line.strip().split('[')[0]
+            else:
+                name = line.strip().split(' ')[0]
+
+            name = name.strip()
+
+            if name and name not in doubans:
+                doubans.add(name)
+            else:
+                print('dup: {}'.format(name))
+
+    with open('README.md', 'r', encoding='utf-8') as f:
+        start = False
+
+        for line in f:
+            if line.startswith('## '):
+                if '看过的电影' in line:
+                    start = True
+                else:
+                    start = False
+
+                continue
+
+            if start:
+                if line.startswith('1. '):
+                    name = line.strip().strip('1. ')
+
+                    if not name:
+                        continue
+
+                    if name not in reads:
+                        reads.add(name)
+                    else:
+                        print('dup2: {}'.format(name))
+
+    print('douban({}), read({})\ndiff1:\n{} \ndiff2:\n{}'.
+          format(len(doubans), len(reads), '\n'.join(doubans - reads), '\n'.join(reads - doubans)))
+
+
 def print_sep():
     print('#' * 25)
 
 
 if __name__ == '__main__':
-    count_sum()
+    # count_sum()
+    find_diff()
